c396923096a7841a9cc3197cd30eb8922c01f7fe
Veniamin <ma3ujia@list.ru>
2021-03-09 18:55:39 +0500

    Vendor phpunit and fix

--- public/index.php
+++ public/index.php
@@ -2,13 +2,14 @@
 session_start();
 
 use app\engine\Render;
+use app\engine\TwigRender;
 use app\engine\Request;
 use app\engine\Autoload;
 
 //TODO сделать путь абсолютным
 include "../config/config.php";
-include "../engine/Autoload.php";
-//include "../vendor/autoload.php";
+//include "../engine/Autoload.php";
+include "../vendor/autoload.php";
 
 try {
     spl_autoload_register([new Autoload(), 'loadClass']);
@@ -21,7 +22,7 @@ try {
     $controllerClass = CONTROLLER_NAMESPACE . ucfirst($controllerName) . "Controller";
 
     if (class_exists($controllerClass)) {
-        $controller = new $controllerClass(new Render());
+        $controller = new $controllerClass(new TwigRender());
         $controller->runAction($actionName);
     }
 
